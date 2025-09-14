7cdf2ee389381e3836176ae5154356e0ba6c54c9
rijalularif <rijalul.arif@gmail.com>
2025-09-14 22:34:20 +0700

    add footer

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -21,6 +21,14 @@
             </div>
         </main>
     </div>
+
+    <footer class="p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800">
+        <div class="mx-auto max-w-screen-xl text-center">
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© <a href="https://rijalularif.github.io/"
+                    class="hover:underline">Rijalul Arif</a>. All Rights Reserved.</span>
+        </div>
+    </footer>
 </body>
+
 
 </html>
