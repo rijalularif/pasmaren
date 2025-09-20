8bdcb8d57af545f15ad32cb77b5b920c234d67b7
Rijalul Arif <74054304+rijalularif@users.noreply.github.com>
2025-09-20 20:54:52 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,13 +12,13 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'name' => 'Name',
-            'username' => 'username',
-            'email' => 'email',
-            'email_verified_at' => now(),
-            'password' => Hash::make('password'),
-            'remember_token' => Str::random(10)
+        // User::create([
+        //     'name' => 'Name',
+        //     'username' => 'username',
+        //     'email' => 'email',
+        //     'email_verified_at' => now(),
+        //     'password' => Hash::make('password'),
+        //     'remember_token' => Str::random(10)
         ]);
     }
 }
